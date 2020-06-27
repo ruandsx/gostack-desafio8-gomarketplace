15e08af686a6b8709c328f719c7c601574006621
Ruan Xavier <ruandsxavier@gmail.com>
2020-06-27 10:06:24 -0300

    Adding cart infos (total items | total price )

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -48,14 +48,22 @@ const Cart: React.FC = () => {
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const subtotal = product.quantity * product.price;
 
-    return formatValue(0);
+      return accumulator + subtotal;
+    }, 0);
+    return formatValue(total);
   }, [products]);
 
-  const totalItensInCart = useMemo(() => {
+  const totalItemsInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const { quantity } = product;
 
-    return 0;
+      return accumulator + quantity;
+    }, 0);
+    return total;
   }, [products]);
 
   return (
@@ -107,7 +115,7 @@ const Cart: React.FC = () => {
       </ProductContainer>
       <TotalProductsContainer>
         <FeatherIcon name="shopping-cart" color="#fff" size={24} />
-        <TotalProductsText>{`${totalItensInCart} itens`}</TotalProductsText>
+        <TotalProductsText>{`${totalItemsInCart} itens`}</TotalProductsText>
         <SubtotalValue>{cartTotal}</SubtotalValue>
       </TotalProductsContainer>
     </Container>
